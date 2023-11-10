4266c47564b73a5367dd6dec83f40da2c0d1c6c3
Kabourlix <hdamaia@outlook.fr>
2023-11-11 00:32:32 +0100

    Add dummy script for testing

---
+++ GameOffProject/RPackages/RContent/Levels/Scenes/Proto/Hugo/DummyPlayer.cs
@@ -0,0 +1,21 @@
+﻿// Created by Kabourlix Cendrée on 11/11/2023
+
+#nullable enable
+
+using System;
+using SDKabu.KCharacter;
+using UnityEngine;
+
+namespace Rezoskour.Content.Levels.Scenes.Proto.Hugo
+{
+    public class DummyPlayer : MonoBehaviour, IKActor
+    {
+        public Guid ID { get; } = Guid.NewGuid();
+        public KHealthComp HealthComp { get; private set; } = null!;
+
+        private void Awake()
+        {
+            HealthComp = new KHealthComp(2000, this);
+        }
+    }
+}
---
+++ GameOffProject/RPackages/RContent/Levels/Scenes/Proto/Hugo/DummySpawner.cs
@@ -0,0 +1,27 @@
+﻿// Created by Kabourlix Cendrée on 11/11/2023
+
+#nullable enable
+
+using System;
+using SDKabu.KCore;
+using UnityEngine;
+
+namespace Rezoskour.Content.Levels.Scenes.Proto.Hugo
+{
+    public class DummySpawner : MonoBehaviour
+    {
+        private IEnemyManager? manager;
+
+        private void Start()
+        {
+            manager = KServiceInjection.Get<IEnemyManager>();
+            if (manager == null)
+            {
+                Debug.LogError("No enemy manager found");
+                return;
+            }
+
+            manager.SpawnEnemy(EnemyType.Basic, Vector2.zero, Quaternion.identity);
+        }
+    }
+}
