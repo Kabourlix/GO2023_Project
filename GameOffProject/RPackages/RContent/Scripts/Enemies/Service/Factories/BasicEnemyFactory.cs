fb15d1c2d25da27d6437c0eef31bd8fb18b1fc37
Kabourlix <hdamaia@outlook.fr>
2023-11-10 22:20:06 +0100

    [Art] Add basic sprite for testing

---
+++ GameOffProject/RPackages/RContent/Scripts/Enemies/Service/Factories/BasicEnemyFactory.cs
@@ -0,0 +1,25 @@
+﻿// Created by Kabourlix Cendrée on 10/11/2023
+
+#nullable enable
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Rezoskour.Content
+{
+    internal class BasicEnemyFactory : EnemyFactory
+    {
+        public BasicEnemyFactory(GameObject _enemyPrefab, int _defaultCapacity, int _maxCapacity,
+            Transform _enemyParent) :
+            base(_enemyPrefab, _enemyParent, _defaultCapacity, _maxCapacity)
+        {
+        }
+
+        protected override Enemy OnCreateEnemy()
+        {
+            Enemy? enemy = Object.Instantiate(enemyPrefab, enemyParent).GetComponent<Enemy>();
+
+            return enemy;
+        }
+    }
+}
