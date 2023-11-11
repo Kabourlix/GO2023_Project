2114ca26e21a4bd1c80e99d0f6d912e417e755ca
Kabourlix <hdamaia@outlook.fr>
2023-11-11 07:28:16 +0100

    [Shop] Add shop basic interface for objects

---
+++ GameOffProject/RPackages/RContent/Scripts/Shop/IObject.cs
@@ -0,0 +1,13 @@
+﻿// Created by Kabourlix Cendrée on 11/11/2023
+
+using System;
+
+namespace Rezoskour.Content.Shop
+{
+    public interface IObject
+    {
+        public string ObjectName { get; }
+        public string ObjectDescription { get; }
+        public string ObjectIconPath { get; }
+    }
+}
