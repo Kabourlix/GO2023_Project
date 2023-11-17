e3cfef8de3690498497e53d3bd65ea9de9918e18
Kabourlix <hdamaia@outlook.fr>
2023-11-17 19:42:13 +0100

    [Shop] Setup architecture

---
+++ GameOffProject/RPackages/RContent/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,21 @@
+﻿// Created by Kabourlix Cendrée on 12/11/2023
+
+using UnityEngine;
+
+namespace Rezoskour.Content.Shop
+{
+    [CreateAssetMenu(fileName = "ShopItem", menuName = "Rezoskour/Shop Item", order = 0)]
+    public class ShopItem : ScriptableObject, IShopItem
+    {
+        [SerializeField] private string objectName;
+        [SerializeField] private string objectDescription;
+        [SerializeField] private string objectIconPath;
+        [SerializeField] private int price;
+
+        public string ObjectName => objectName;
+        public string ObjectDescription => objectDescription;
+        public string ObjectIconPath => objectIconPath;
+        public int Price => price;
+        public bool IsAvailable { get; set; }
+    }
+}
