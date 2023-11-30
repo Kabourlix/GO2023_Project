b40265bba2d74d39a5326961c853442fea432b47
Kabourlix <hdamaia@outlook.fr>
2023-12-01 00:07:23 +0100

    Add dash

--- GameOffProject/RPackages/RContent/Scripts/GameLogic/GameManager.cs
+++ GameOffProject/RPackages/RContent/Scripts/GameLogic/GameManager.cs
@@ -1,6 +1,7 @@
 // Created by Kabourlix Cendrée on 11/11/2023
 
 using System;
+using Rezoskour.Content.Shop;
 using SDKabu.KCore;
 using UnityEngine;
 
@@ -9,15 +10,19 @@ namespace Rezoskour.Content
     public interface IGameManager : IKService
     {
         public Transform PlayerTransform { get; }
+
+        public ICurrencyUser PlayerCurrency { get; }
     }
 
     public class GameManager : IGameManager
     {
         public Transform PlayerTransform { get; }
+        public ICurrencyUser PlayerCurrency { get; }
 
         public GameManager(Transform _playerTransform)
         {
             PlayerTransform = _playerTransform;
+            PlayerCurrency = _playerTransform.GetComponent<PlayerBrain>();
         }
 
         public void Dispose()
